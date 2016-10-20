48845132da4b7460c375d17b59c41fbb05306c52
Mirco Gysin <miagox@gmail.com>
2016-10-20 18:24:56 +0200

    initial commit

---
+++ src/main.c
@@ -0,0 +1,33 @@
+/* 
+ * This file is part of the Graffiti distribution 
+ * (https://github.com/miago/graffiti).
+ * Copyright (c) 2016 Mirco Gysin.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file main.c
+ * @author Mirco E. Gysin
+ * @date 20 Oct 2016
+ * @brief This is the main file of the Temporary Graffity project 
+ */
+  
+#include "stm32f10x.h"
+
+ 
+ 
+ int main(void) {
+ 
+}
+ 
